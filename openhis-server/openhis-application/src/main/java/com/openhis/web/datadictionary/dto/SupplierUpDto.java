a9b01023bcc7d635f174161d8b81429fab4aaa18
duhe <duhe@ccbjb.com.cn>
2025-02-28 17:37:14 +0800

    供应商后台 up by dh

--- openhis-server/openhis-application/src/main/java/com/openhis/web/datadictionary/dto/SupplierUpDto.java
+++ openhis-server/openhis-application/src/main/java/com/openhis/web/datadictionary/dto/SupplierUpDto.java
@@ -10,6 +10,9 @@ import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -21,34 +24,42 @@ import java.util.Date;
  */
 @Data
 @Accessors(chain = true)
-public class SupplierDto {
+public class SupplierUpDto {
 
     /** ID */
     @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     /** 编号 */
+    @NotBlank(message = "编号不能为空")
     private String busNo;
 
     /** 名称 */
+    @NotBlank(message = "名称不能为空")
     private String name;
 
     /** 类型 */
+    @NotNull(message = "类型不能为空")
     private Integer typeEnum;
 
     /** 地址 */
+    @NotBlank(message = "地址不能为空")
     private String address;
 
     /** 拼音码 */
+    @NotBlank(message = "拼音码不能为空")
     private String pyStr;
 
     /** 五笔码 */
+    @NotBlank(message = "五笔码不能为空")
     private String wbStr;
 
     /** 联系人电话 */
+    @NotBlank(message = "联系人电话不能为空")
     private String phone;
 
     /** 联系人邮箱 */
+    @NotBlank(message = "联系人邮箱不能为空")
     private String email;
 
     /** 活动标识 */
--- openhis-server/openhis-domain/src/main/java/com/openhis/administration/service/impl/SupplierServiceImpl.java
+++ openhis-server/openhis-domain/src/main/java/com/openhis/administration/service/impl/SupplierServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.core.common.utils.StringUtils;
 import com.core.common.utils.bean.BeanUtils;
+import com.openhis.clinical.domain.ConditionDefinition;
+import com.openhis.clinical.mapper.ConditionDefinitionMapper;
 import com.openhis.common.enums.DelFlag;
 import com.openhis.workflow.domain.SupplyRequest;
 import lombok.AllArgsConstructor;
@@ -14,6 +16,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.openhis.administration.domain.Supplier;
 import com.openhis.administration.mapper.SupplierMapper;
 import com.openhis.administration.service.ISupplierService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 供应商管理Service业务层处理
@@ -25,43 +30,28 @@ import com.openhis.administration.service.ISupplierService;
 @Service
 @AllArgsConstructor
 public class SupplierServiceImpl extends ServiceImpl<SupplierMapper, Supplier> implements ISupplierService {
-
     private final SupplierMapper supplierMapper;
     /**
-     * 查询厂商/产地列表
+     * 新增供应商
      *
-     * @param supplier 查询条件
-     * @param pageNo 查询条件
-     * @param pageSize 查询条件
-     * @return 厂商/产地列表
+     * @param supplier 厂商/产地实体
+     * @return
      */
     @Override
-    public Page<Supplier> getPage(Supplier supplier, Integer pageNo, Integer pageSize) {
-        Page<Supplier> supplierList = new Page<>();
-//        // 判断入库数量
-//        if (supplyRequest.getItemQuantity() != 0) {
-//            // 查询供应申请信息列表
-//            supplyRequestList = supplyRequestMapper.selectPage(new Page<>(pageNo, pageSize),
-//                    new LambdaQueryWrapper<SupplyRequest>().eq(SupplyRequest::getDeleteFlag, DelFlag.NO.getValue()));
-//        } else {
-//            return null;
-//        }
-
-        // 设置查询条件
-        LambdaQueryWrapper<Supplier> lambdaQueryWrapper = new LambdaQueryWrapper<>();
-        if (StringUtils.isNotEmpty(supplier.getBusNo())) {
-            lambdaQueryWrapper.eq(Supplier::getBusNo, supplier.getBusNo());
+    @Transactional(rollbackFor = Exception.class)
+    public boolean addSupplier(Supplier supplier) {
+        // 根据病种编码判断病种是否存在
+        List<Supplier> suppliers =
+                supplierMapper.selectList(new LambdaQueryWrapper<Supplier>()
+                        .eq(Supplier::getBusNo, supplier.getBusNo()));
+        if (suppliers.size() > 0) {
+            return false;
         }
-        if (StringUtils.isNotEmpty(supplier.getName())) {
-            lambdaQueryWrapper.eq(Supplier::getName, supplier.getName());
+        // 新增供应商
+        int insert = supplierMapper.insert(supplier);
+        if (insert != 1) {
+            return false;
         }
-        if (StringUtils.isNotNull(supplier.getTypeEnum())) {
-            lambdaQueryWrapper.eq(Supplier::getTypeEnum, supplier.getTypeEnum());
-        }
-        lambdaQueryWrapper.eq(Supplier::getDeleteFlag, DelFlag.NO.getValue());
-
-        supplierList = supplierMapper.selectPage(new Page<>(pageNo, pageSize), lambdaQueryWrapper);
-        // 返回入库信息列表
-        return supplierList;
+        return true;
     }
 }
