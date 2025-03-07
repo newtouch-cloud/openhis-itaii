ea2405595f2393934642a1443146224873ec45b7
Wang.Huan <wanghuan@ccbjb.com.cn>
2025-03-07 13:42:03 +0800

    生成就诊序号

--- openhis-server/openhis-domain/src/main/java/com/openhis/administration/service/impl/EncounterServiceImpl.java
+++ openhis-server/openhis-domain/src/main/java/com/openhis/administration/service/impl/EncounterServiceImpl.java
@@ -1,9 +1,13 @@
 package com.openhis.administration.service.impl;
 
+import javax.annotation.Resource;
+
+import com.core.common.enums.AssignSeqEnum;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.core.common.utils.AssignSeqUtil;
 import com.openhis.administration.domain.Encounter;
 import com.openhis.administration.mapper.EncounterMapper;
 import com.openhis.administration.service.IEncounterService;
@@ -18,6 +22,9 @@ import com.openhis.common.enums.EncounterType;
 @Service
 public class EncounterServiceImpl extends ServiceImpl<EncounterMapper, Encounter> implements IEncounterService {
 
+    @Resource
+    AssignSeqUtil assignSeqUtil;
+
     /**
      * 保存就诊信息
      *
@@ -26,10 +33,11 @@ public class EncounterServiceImpl extends ServiceImpl<EncounterMapper, Encounter
      */
     @Override
     public Long saveEncounterByRegister(Encounter encounter) {
-        // 生产就诊编码
-
-        // 生产就诊序号
-
+        // 生成就诊编码
+        encounter.setBusNo(assignSeqUtil.getSeq(AssignSeqEnum.ENCOUNTER_NUM.getPrefix(), 8));
+        // 生成就诊序号 (患者ID + 科室ID 作为当日就诊号的唯一标识)
+        String preFix = encounter.getPatientId() + String.valueOf(encounter.getOrganizationId());
+        encounter.setDisplayOrder(assignSeqUtil.getSeqNoByDay(preFix));
         // 患者ID
         Long patientId = encounter.getPatientId();
         // 初复诊
