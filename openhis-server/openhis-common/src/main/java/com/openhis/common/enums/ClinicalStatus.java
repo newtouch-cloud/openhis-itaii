278994c46a2e5eec456ba7ef85c3e20bf0cfadc8
liuhongrui <liuhr@ccbjb.com.cn>
2025-03-07 09:30:59 +0800

    新增过敏与不耐受

---
+++ openhis-server/openhis-common/src/main/java/com/openhis/common/enums/ClinicalStatus.java
@@ -0,0 +1,25 @@
+package com.openhis.common.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 过敏与不耐受中临床状况
+ *
+ * @author liuhr
+ * @date 2025/3/6
+ */
+@Getter
+@AllArgsConstructor
+public enum ClinicalStatus implements HisEnumInterface  {
+    ACTIVE(1, "active", "阳性"),
+    INACTIVE(2, "inactive", "阴性"),
+    RESOLVED(3, "resolved", "已解决");
+
+    @EnumValue
+    private final Integer value;
+    private final String code;
+    private final String info;
+
+}
