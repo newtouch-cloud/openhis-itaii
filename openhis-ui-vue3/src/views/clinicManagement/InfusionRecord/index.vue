8d1b7ff6ca23aeda58e36c21beda961271212c86
菲菲 <12704587+yvwen@user.noreply.gitee.com>
2025-03-14 17:38:43 +0800

    输液

--- openhis-ui-vue3/src/views/clinicManagement/InfusionRecord/index.vue
+++ openhis-ui-vue3/src/views/clinicManagement/InfusionRecord/index.vue
@@ -4,7 +4,7 @@
 			<el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch">
            		<el-form-item label="入院日期" prop="patientname">
             		<el-date-picker v-model="dateRange"  value-format="YYYY-MM-DD" type="daterange" range-separator="-" 
-            		start-placeholder="开始日期" end-placeholder="结束日期" ></el-date-picker>
+            		start-placeholder="开始日期" end-placeholder="结束日期" style="width: auto;"></el-date-picker>
 		   		</el-form-item>
 		   		<el-form-item label="" prop="phone">
 		      		<el-input v-model="queryParams.phone"  placeholder="门诊号/病人/ID" clearable style="width: 180px"
@@ -17,18 +17,19 @@
 			</el-form>
 
 			<el-table :data="patientList" border style="width: 100%" highlight-current-row @current-change="handleCurrentChange" >
-				<el-table-column label="操作" align="center" width="90" fixed="right">
+				<!-- <el-table-column label="操作" align="center" width="90" fixed="right">
 					<template #default="scope">
 							<el-button link type="primary" icon="Edit" @click="handlePrescription(scope.row)" v-hasPermi="['system:menu:edit']">处方</el-button>
 					</template>
-				</el-table-column>
-				<el-table-column prop="busNo" label="处方号" width="160" />
-				<el-table-column prop="name" label="病人" width="120" />
-				<el-table-column prop="genderEnum" label="开方时间" width="140" />
-				<el-table-column prop="status" label="状态" width="140" />
-				<el-table-column prop="doctorName" label="开方医生" width="140" />
+				</el-table-column> -->
+				<el-table-column prop="busNo" label="处方号" width="150" />
+				<el-table-column prop="doctorName" label="姓名" width="100" />
+				<el-table-column prop="name" label="性别" width="80" /> 
+				<el-table-column prop="name" label="年龄" width="80" />
+				<el-table-column prop="status" label="身份证号" width="140" />
 		  </el-table>
-		  <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" style="margin-left: 15px;" />
+		  <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" 
+			v-model:limit="queryParams.pageSize" @pagination="getList" />
 		</div>
 
 		<div class="right">
@@ -45,23 +46,7 @@
 			</el-form>
 			<div>
 				<p style="margin: 0px 0px 10px 0px;">院注医嘱</p>
-			<el-table :data="outpatienRecordsList" border style="width: 100%;height: 300px;">
-				<el-table-column prop="name" label="院注次数" width="180" />
-				<el-table-column prop="idCard" label="已确认次数" width="180" />
-				<el-table-column prop="description" label="开立时间" width="180" />
-				<el-table-column prop="patientBusNo" label="开单医生" width="180" />
-				<el-table-column prop="encounterBusNo" label="科别" width="180" />
-				<el-table-column prop="genderEnum_enumText" label="性别" width="80" />
-				<el-table-column prop="phone" label="医嘱" width="160" />
-				<el-table-column prop="encounterTime" label="组" width="180" />
-				<el-table-column prop="subjectStatusEnum_enumText" label="频次" width="120" />
-				<el-table-column prop="organizationName" label="每次量" width="180" />
-				<el-table-column prop="doctorName" label="用法" width="180" />
-			</el-table>
-			</div>
-			<div>
-				<p style="margin: 13px 0px 10px 0px;">院注执行历史</p>
-				<el-table :data="outpatienRecordsList" border style="width: 100%;max-height: 250px;">
+				<el-table :data="outpatienRecordsList" border style="width: 100%;height: 300px;">
 					<el-table-column prop="name" label="院注次数" width="180" />
 					<el-table-column prop="idCard" label="已确认次数" width="180" />
 					<el-table-column prop="description" label="开立时间" width="180" />
@@ -71,8 +56,24 @@
 					<el-table-column prop="phone" label="医嘱" width="160" />
 					<el-table-column prop="encounterTime" label="组" width="180" />
 					<el-table-column prop="subjectStatusEnum_enumText" label="频次" width="120" />
-					<el-table-column prop="organizationName" label="每次量" width="180" />
+					<el-table-column prop="organizationName" label="每次量" width="180" /> 
 					<el-table-column prop="doctorName" label="用法" width="180" />
+				</el-table>
+			</div>
+			<div>
+				<p style="margin: 13px 0px 10px 0px;">院注执行历史</p>
+				<el-table :data="outpatienRecordsList" border style="width: 100%;max-height: 250px;">
+					<el-table-column prop="name" label="执行时间" width="150" />
+					<el-table-column prop="genderEnum_enumText" label="执行人" width="80" />
+					<el-table-column prop="name" label="患者姓名" width="100" />
+					<el-table-column prop="idCard" label="开单医生" width="100" />
+					<el-table-column prop="description" label="科别" width="120" />
+					<el-table-column prop="phone" label="医嘱" width="160" />
+					<el-table-column prop="encounterTime" label="组" width="50" />
+					<el-table-column prop="subjectStatusEnum_enumText" label="频次" width="80" />
+					<el-table-column prop="organizationName" label="每次量" width="80" />
+					<el-table-column prop="doctorName" label="用法" width="160" />
+					<el-table-column prop="encounterBusNo" label="医嘱备注" width="180" />
 				</el-table>
 			</div>
 		</div>
@@ -165,13 +166,13 @@ const data = reactive({
 const { queryParams } = toRefs(data);
 
 /** 查询门诊输液列表 */
-// function getList() {
+function getList() {
 //   listJobLog(proxy.addDateRange(queryParams.value, dateRange.value)).then(response => {
 //     jobLogList.value = response.rows;
 //     total.value = response.total;
 //     loading.value = false;
 //   });
-// }
+}
 
 /** 搜索按钮操作 */
 function handleQuery() {
@@ -180,6 +181,7 @@ function handleQuery() {
 }
 /** 重置按钮操作 */
 function resetQuery() {
+  dateRange.value = [];
   proxy.resetForm("queryRef");
   handleQuery();
 }
@@ -198,7 +200,7 @@ function handleCurrentChange(row) {
 	  medicineData.value = [];
     }
 
-// getList();
+getList();
 
 </script>
 
@@ -207,13 +209,13 @@ function handleCurrentChange(row) {
   	display: flex;
 }
 .left {
-  	width: 27%;
+  	width: 28%;
 }
 .right {
-	margin-left: 5%;
+	margin-left: 2%;
   	width: 72%;
 }
 .el-table__row--current {
-  background-color: cyan !important; /* 青色背景 */
+  background-color: cyan ; /* 青色背景 */
 }
 </style>
