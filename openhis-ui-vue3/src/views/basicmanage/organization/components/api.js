dd8a8f00b173247d79215d4115498dbe1bf29a0f
Zhang.YC <zhangyc@ccbjb.com.cn>
2025-03-25 18:29:33 +0800

    科室接口修改

--- openhis-ui-vue3/src/views/basicmanage/organization/components/api.js
+++ openhis-ui-vue3/src/views/basicmanage/organization/components/api.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getList(queryParams) {
   return request({
-    url: '/basedatamanage/organization/organization',
+    url: '/base-data-manage/organization/organization',
     method: 'get',
     param: queryParams
   })
@@ -10,7 +10,7 @@ export function getList(queryParams) {
 
 export function addOrganization(data) {
   return request({
-    url: '/basedatamanage/organization/organization',
+    url: '/base-data-manage/organization/organization',
     method: 'post',
     data: data
   })
@@ -18,7 +18,7 @@ export function addOrganization(data) {
 
 export function updateOrganization(data) {
   return request({
-    url: '/basedatamanage/organization/organization',
+    url: '/base-data-manage/organization/organization',
     method: 'put',
     data: data
   })
@@ -26,7 +26,7 @@ export function updateOrganization(data) {
 
 export function deleteOrganization(param) {
   return request({
-    url: '/basedatamanage/organization/organization',
+    url: '/base-data-manage/organization/organization',
     method: 'delete',
     params: param
   })
@@ -34,28 +34,28 @@ export function deleteOrganization(param) {
 
 export function getOrgDetail(id) {
   return request({
-    url: '/basedatamanage/organization/organization?orgId=' + id,
+    url: '/base-data-manage/organization/organization?orgId=' + id,
     method: 'get',
   })
 }
 
 export function initOrgTypeOption() {
   return request({
-    url: '/basedatamanage/organization/init',
+    url: '/base-data-manage/organization/init',
     method: 'get',
   })
 }
 
 export function disableOrg(id) {
   return request({
-    url: '/basedatamanage/organization/organization-inactive?orgId=' + id,
+    url: '/base-data-manage/organization/organization-inactive?orgId=' + id,
     method: 'put',
   })
 }
 
 export function enableOrg(id) {
   return request({
-    url: '/basedatamanage/organization/organization-active?orgId=' + id,
+    url: '/base-data-manage/organization/organization-active?orgId=' + id,
     method: 'put',
   })
 }
